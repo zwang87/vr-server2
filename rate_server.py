f6129ee0fd388f7de866593dd795bec177b260a2
Zach Cimafonte <zach@cimafonte.com>
2015-04-08 21:58:19 -0400

    reorg

--- rate_server.py
+++ rate_server.py
@@ -6,28 +6,33 @@ import logging
 from io import BytesIO
 import time
 
-CONSTANT = .001
-print CONSTANT
-
 class XmlRelay(DatagramProtocol):
-	def __init__(self):
-		self.sendXmlTimer = None
-		self.clients = [("192.168.1.3", 1610)]
-		self.frame_data = ''
+        def __init__(self, _sc):
+                self.sc = _sc
+                self.sendXmlTimer = None
+                self.clients = [("192.168.1.2", 1610)]
+                self.frame_data = None
 
-	def datagramReceived(self, data, (in_host, in_port)):
-		self.frame_data = data
-		self.sendXml()
+        def datagramReceived(self, data, (in_host, in_port)):
+                #print data
+                self.frame_data = data
+                self.sendXml()
 
-	def sendXml(self):
-		if self.sendXmlTimer and self.sendXmlTimer.active():
-			self.sendXmlTimer.reset(CONSTANT)
-		else:
-			self.sendXmlTimer = reactor.callLater(CONSTANT, self.sendXml)
-		for host, port in self.clients:
-			self.transport.write(self.frame_data, (host, port))
+        def sendXml(self):
+                # Every 10 milliseconds
+                if self.sendXmlTimer and self.sendXmlTimer.active():
+                        self.sendXmlTimer.reset(.01)
+                else:
+                        self.sendXmlTimer = reactor.callLater(.01, self.sendXml)
+                for host, port in self.clients:
+                        sc.sendMsg(self.frame_data, (host, port))
 
-relay = XmlRelay()
-reactor.listenUDP(1615, relay)
-relay.sendXmlTimer = reactor.callLater(CONSTANT, relay.sendXml)
+class ServerConnection(DatagramProtocol):
+     def sendMsg(self, data, (host, port)):
+         self.transport.write(data, (host, port))
+ 
+sc = ServerConnection()
+relay = XmlRelay(sc)
+reactor.listenUDP(0, sc, interface='192.168.1.11', maxPacketSize=65507)
+reactor.listenUDP(1609, relay, interface='127.0.0.1', maxPacketSize=65507)
 reactor.run()
