481d3d26fa8d9e34a79c22a6bcd9ed9bdb1f4ebf
Zach Cimafonte <zach@cimafonte.com>
2015-04-09 00:01:38 -0400

    chalktalk

--- servercat.py
+++ servercat.py
@@ -3,7 +3,7 @@ import struct
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('192.168.1.3', 1611))
+sock.bind(('127.0.0.1', 1615))
 
 while True:
 	#print "%d %02f %02f %02f %02f %02f %02f %02f" % struct.unpack("ifffffff", sock.recv(32))
