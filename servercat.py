af0febb06397433cf436ed768387fcad46cf460e
Zach Cimafonte <zach@cimafonte.com>
2015-05-03 13:33:34 -0400

    protocol buffers

--- servercat.py
+++ servercat.py
@@ -1,12 +1,26 @@
 import socket
 import struct
+from time import time
+
+def cur_milliseconds():
+    return time() * 1000
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(('127.0.0.1', 1611))
+sock.bind(('192.168.1.4', 1611))
 
+i = 0
+last_report = cur_milliseconds()
 while True:
-	print sock.recv(60000)
+	a = sock.recv(60000)
+	#print a
+	i += 1
+	now = cur_milliseconds()
+	if now - last_report >=1000:
+		print i
+		i = 0
+		last_report = now
+
 
 """
 while True:
